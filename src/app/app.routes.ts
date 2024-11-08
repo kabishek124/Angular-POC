1c55945aa9fc23df2e01d5e743a17d340fab762e
kabishek124 <jcoyavkshek@gmail.com>
2024-11-08 13:04:02 +0530

    Slight changes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,6 +6,7 @@ import { ResInfoComponent } from './pages/resInfo/res-info.component';
 
 export const routes: Routes = [
     {path:"", component: HomeComponent},
+    {path:"home", component: HomeComponent},
     {path:"about", component: AboutComponent},
     {path:"contact", component: ContactComponent},
     {path:"restaurant/:id", component: ResInfoComponent}
